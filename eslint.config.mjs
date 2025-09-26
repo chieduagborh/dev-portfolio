437d796f3f7ed303a473d99fe7921be287b163dc
Chiedu Agborh <chiedu14@gmail.com>
2025-09-26 16:13:29 +0100

    fixed eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,14 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+   rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      "@typescript-eslint/no-explicit-any": "off",
+      "@next/next/no-img-element":"off"
+    },
+  }
 ];
 
 export default eslintConfig;
